16d0fdb6ef126a60ea62494009b8c3caef027f78
SarawadeShrutika <113629567+SarawadeShrutika@users.noreply.github.com>
2022-10-18 10:59:46 -0700

    MarkONe src file

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Mark2_Src
- 
+ Neog:MarkOne
+This contains Code For Where you have been asked Five Questions About me You need to answer them and get the score.
---
+++ index.js
@@ -0,0 +1,80 @@
+var readline = require('readline-sync');
+var score = 0;
+
+var Questions = [{
+  questions: "Which School I have been to?",
+  answer: "St Jude High School",
+  options: [
+    "Shivaji Vidyalay",
+    "St Joseph",
+    "St Jude High School",
+    "Wisdom School"]
+
+}, {
+  questions: "Which is my favourite vegetable?",
+  answer: "Bhendi",
+  options: [
+    "Gavar",
+    "Bhendi",
+    "Beans",
+    "Brinjal"]
+}, {
+  questions: "Where Do I work?",
+  answer: "Pieriandx",
+  options: [
+    "Wipro",
+    "Yardi",
+    "Capgemini",
+    "Pieriandx"]
+}, {
+  questions: "Which City Do I live?",
+  answer: "Pune",
+  options: [
+    "Chennai",
+    "Bangalore",
+    "Pune",
+    "Mumbai"]
+}];
+
+
+function welcome() {
+  console.log("****Hello!!! welcome to quick quizzz APP****");
+  var username = readline.question("Please Can you enter your name? ");
+  console.log("WELCOME " + username.toUpperCase() + "!!!! LETS CHECK HOW MUCH YOU KNOW ME!!!");
+  Start();
+}
+
+function play() {
+  for (var i = 0; i < Questions.length; i++) {
+    var quest = Questions[i];
+    game(quest.questions, quest.answer, quest.options);
+  }
+
+}
+function Start() {
+  var start = readline.question("Please Type Start to start the game!!!");
+  if (start) {
+    play();
+  }
+}
+function game(questions, answer, options) {
+  console.log(questions);
+  for (var i = 0; i < options.length; i++) {
+    var option = options[i];
+    console.log(option);
+  }
+  var user_answer = readline.question("Please enter the answer from the above options");
+  if (user_answer == answer) {
+    score = score + 1;
+  }
+}
+function finalscore(score) {
+  if (score == 2) {
+    console.log("BRAVO!!!! You WON");
+  } else {
+    console.log("Better luck next time!!!");
+  }
+}
+welcome();
+
+finalscore(score);
